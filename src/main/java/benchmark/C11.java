35320f1581629ffe404e791e1a399c13d7b17e07
Andreas Voellmy <andreasvoellmy@forwardnetworks.com>
2017-07-15 16:24:46 -0700

    First commit

---
+++ src/main/java/benchmark/C11.java
@@ -0,0 +1,26 @@
+package benchmark;
+import org.derive4j.Data;
+@Data
+ public abstract class C11
+{
+  public interface Cases <R>
+  {
+    R m1 (Integer p1)
+    ;
+    R m2 (Integer p1, String p2)
+    ;
+    R m3 (Integer p1, String p2, Object p3)
+    ;
+  }
+  public abstract <R> R match (Cases<R> cases)
+  ;
+  @Override
+  public abstract String toString ()
+  ;
+  @Override
+  public abstract boolean equals (Object other)
+  ;
+  @Override
+  public abstract int hashCode ()
+  ;
+}
